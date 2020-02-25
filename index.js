d8be6a222cc2fe876be8f053501175043c8a2880
elvinzhu <yongyu168@foxmail.com>
2020-02-25 18:27:23 +0800

    refactor

--- index.js
+++ index.js
@@ -1,28 +1,6 @@
 
-const $ = require('jquery');
+const mockEventBinding = require('./src/ mockEventBinding');
 
-function mockEventBinding() {
-  // use jquery to bind/remove event
-  const oldBinder = HTMLElement.prototype.addEventListener;
-  const oldRemove = HTMLElement.prototype.removeEventListener;
-
-  const newBinder = function (type, callback) {
-    this.addEventListener = oldBinder;
-    $(this).on(type, callback);
-    this.addEventListener = newBinder;
-  }
-
-  const newRemove = function (type, callback) {
-    this.removeEventListener = oldRemove;
-    $(this).off(type, callback);
-    this.removeEventListener = newRemove;
-  }
-
-  HTMLElement.prototype.addEventListener = newBinder;
-  HTMLElement.prototype.removeEventListener = newRemove;
-
-  window.addEventListener = newBinder;
-  window.removeEventListener = newRemove;
+module.exports = {
+  mockEventBinding
 }
-
-exports.mockEventBinding = mockEventBinding;
---
+++ src/ mockEventBinding.js
@@ -0,0 +1,25 @@
+const $ = require('jquery');
+
+module.exports = function mockEventBinding() {
+  // use jquery to bind/remove event
+  const oldBinder = HTMLElement.prototype.addEventListener;
+  const oldRemove = HTMLElement.prototype.removeEventListener;
+
+  const newBinder = function (type, callback) {
+    this.addEventListener = oldBinder;
+    $(this).on(type, callback);
+    this.addEventListener = newBinder;
+  }
+
+  const newRemove = function (type, callback) {
+    this.removeEventListener = oldRemove;
+    $(this).off(type, callback);
+    this.removeEventListener = newRemove;
+  }
+
+  HTMLElement.prototype.addEventListener = newBinder;
+  HTMLElement.prototype.removeEventListener = newRemove;
+
+  window.addEventListener = newBinder;
+  window.removeEventListener = newRemove;
+}
